69eab3b93a2988989faf99144271f1b8816299ea
Aman Raj Rana <mail2amanrana@gmail.com>
2023-05-15 16:44:51 +0530

    Make UserAccount menu responsive
    	new file:   static/script/userAccount/template.js

---
+++ static/script/userAccount/template.js
@@ -0,0 +1,42 @@
+// ===================  Responsive Code ============================= //
+
+function wideScreenSize() {
+    const menuItem = ['Home', 'Your urls', 'Create New', 'Order service', 'Notifications', 'Profile', 'Log out'];
+    const websiteName = "ARR link shortener";
+
+    const logo = document.getElementById('logo');
+    const menu = document.getElementsByClassName('menu-item');
+    const nav = document.getElementsByTagName('nav');
+
+    nav[0].style.width = '275px';
+    logo.innerText = websiteName;
+
+    for (let i = 0; i < menu.length; i++) {
+        menu[i].innerHTML = menuItem[i];
+    }
+}
+
+
+function midScreenSize() {
+    const logo = document.getElementById('logo');
+    const menu = document.getElementsByClassName('menu-item');
+    const nav = document.getElementsByTagName('nav');
+
+    nav[0].style.width = '';
+    logo.innerText = '';
+
+    for (let i = 0; i < menu.length; i++) {
+        menu[i].innerHTML = "";
+    }
+}
+
+function callWidowSizeFunction(currentWindowWidth) {
+    if (currentWindowWidth < 950) {
+        midScreenSize();
+    } else {
+        wideScreenSize();
+    }
+}
+
+window.addEventListener('resize', () => callWidowSizeFunction(window.innerWidth));
+window.addEventListener('load', () => callWidowSizeFunction(window.innerWidth));
